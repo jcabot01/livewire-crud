35daff4c24c605c29f79ec609692c2c9df1d08aa
Jon Cabot <jfc0053@gmail.com>
2024-09-15 17:44:35 -0500

    added form validation to post creation

--- app/Livewire/PostForm.php
+++ app/Livewire/PostForm.php
@@ -2,10 +2,32 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class PostForm extends Component
 {
+    #[Validate('required', message: 'Post title is required')]
+    #[Validate('min:3', message: 'Post title must be 3 chars long')]
+    #[Validate('max:150', message: 'Post title must be 150 chars longs')]
+    public $title;
+
+    #[Validate('required', message: 'Post content is required')]
+    #[Validate('min:10', message: 'Post content must be minimum 10 chars long')]
+    public $content;
+
+    #[Validate('required', message: 'Featured Image is required')]
+    #[Validate('image', message: 'Featured Image must be a valid image')]
+    #[Validate('mimes:jpg,jpeg,png,svg,bmp,webp,gif', message: 'Featured Imag accepts only jpg, jpeg, png, svg, bmp, webp, and gif')]
+    #[Validate('max:2048', message: 'Featured Image must not be larger than 2MB')]
+    public $featuredImage;
+
+
+    public function savePost()
+    {
+        $this->validate();
+    }
+
     public function render()
     {
         return view('livewire.post-form');
--- resources/views/livewire/post-form.blade.php
+++ resources/views/livewire/post-form.blade.php
@@ -1,46 +1,58 @@
 <div class="container pt-5">
     <div class="row">
         <div class="col-8 m-auto">
-
-            <div class="card shadow">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-xl-6">
-                            <h5 class="fw-bold">Create Post</h5>
+            <form wire:submit="savePost">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <h5 class="fw-bold">Create Post</h5>
+                            </div>
+                            <div class="col-xl-6 text-end">
+                                <a wire:navigate href="{{ route('posts')}}" class="btn btn-primary btn-sm">Back to Posts</a>
+                            </div>
                         </div>
-                        <div class="col-xl-6 text-end">
-                            <a wire:navigate href="{{ route('posts')}}" class="btn btn-primary btn-sm">Back to Posts</a>
+                    </div>
+                    <div class="card-body">
+
+                        <!-- Post Title -->
+                        <div class="form-group mb-2">
+                            <label for="title">Title<span class="text-danger">*</span></label>
+                            <input type="text" wire:model="title" class="form-control" id="title" placeholder="Post Title">
+
+                            @error('title')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <!-- Post Content -->
+                        <div class="form-group mb-2">
+                            <label for="content">Content<span class="text-danger">*</span></label>
+                            <textarea class="form-control" wire:model="content" id="content" placeholder="Post Content"></textarea>
+
+                            @error('content')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <!-- Post Featured Image -->
+                        <div class="form-group mb-2">
+                            <label for="featuredImage">Featured Image<span class="text-danger">*</span></label>
+                            <input type="file" wire:model="featuredImage" class="form-control" id="featuredImage">
+
+                            @error('featuredImage')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <div class="form-group mb-2">
+                            <button type="submit" class="btn btn-success">Save</button>
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
-
-                    <!-- Post Title -->
-                    <div class="form-group mb-2">
-                        <label for="title">Title<span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="title" placeholder="Post Title">
-                    </div>
-
-                    <!-- Post Content -->
-                    <div class="form-group mb-2">
-                        <label for="content">Content<span class="text-danger">*</span></label>
-                        <textarea class="form-control" id="content" placeholder="Post Content"></textarea>
-                    </div>
-
-                    <!-- Post Featured Image -->
-                    <div class="form-group mb-2">
-                        <label for="featuredImage">Featured Image<span class="text-danger">*</span></label>
-                        <input type="file" class="form-control" id="featuredImage">
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    <div class="form-group mb-2">
-                        <button type="submit" class="btn btn-primary">Save</button>
-                    </div>
-                </div>
-            </div>
-
+            </form>
         </div>
     </div>
 </div>
